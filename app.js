b6d689c9294567f4727c0775daae0ccaaea8f102
sanromannelyt-bit <sanromannelyt@gmail.com>
2025-11-23 20:44:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 // Import Express.js
 const express = require('express');
+
+// Create an Express app
 const app = express();
 
-// Middleware para leer JSON
+// Middleware to parse JSON bodies
 app.use(express.json());
 
-// Ruta para verificación del webhook
-app.get('/webhook', (req, res) => {
-  const verify_token = "fluentia_token";
+// Set port and verify_token
+const port = process.env.PORT || 3000;
+const verifyToken = process.env.VERIFY_TOKEN;
 
-  const mode = req.query['hub.mode'];
-  const token = req.query['hub.verify_token'];
-  const challenge = req.query['hub.challenge'];
+// Route for GET requests
+app.get('/', (req, res) => {
+  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
 
-  if (mode && token && mode === 'subscribe' && token === verify_token) {
-    console.log("WEBHOOK_VERIFIED");
+  if (mode === 'subscribe' && token === verifyToken) {
+    console.log('WEBHOOK VERIFIED');
     res.status(200).send(challenge);
   } else {
-    res.sendStatus(403);
+    res.status(403).end();
   }
 });
 
-// Ruta para recibir mensajes
-app.post('/webhook', (req, res) => {
-  console.log("Mensaje recibido:");
+// Route for POST requests
+app.post('/', (req, res) => {
+  const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
+  console.log(`\n\nWebhook received ${timestamp}\n`);
   console.log(JSON.stringify(req.body, null, 2));
-  res.sendStatus(200);
+  res.status(200).end();
 });
 
-// Puerto para Render
-app.listen(10000, () => console.log("Webhook running on port 10000"));
+// Start the server
+app.listen(port, () => {
+  console.log(`\nListening on port ${port}\n`);
+});
